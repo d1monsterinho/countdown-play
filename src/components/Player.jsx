479e8057ef1fbdae25236b9da425ac5a4a97df44
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-04-27 16:49:38 +0300

    improved input logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,17 +2,17 @@ import { useRef, useState } from "react";
 
 export default function Player() {
   const playerNameInputRef = useRef(null);
-  const [playerName, setPlayerName] = useState('unknown');
+  const [playerName, setPlayerName] = useState('unknown player');
 
   function handleOnClickSetName() {
-    const playerName = playerNameInputRef.current.value;
-    setPlayerName(playerName);
+    const enteredPlayerName = playerNameInputRef.current.value;
+    setPlayerName(enteredPlayerName);
     playerNameInputRef.current.value = '';
   }
 
   return (
     <section id="player">
-      <h2>Welcome {playerName || 'unknown'}</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
         <input ref={playerNameInputRef} type="text"/>
         <button onClick={handleOnClickSetName}>Set Name</button>
